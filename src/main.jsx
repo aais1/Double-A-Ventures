6057d65ca2edc984c1dbecd12d502da16c73f342
AliAais <aaisali228@gmail.com>
2024-06-01 08:44:00 -0700

    refactor: Update routes in main.jsx for admin dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Route, createBrowserRouter, createRoutesFromElements , RouterProvider } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store/store.js'
-import AdminLayout from './AdminLayout.jsx'
-import { Login } from './pages'
-import { AdminLogin } from './pages/admin'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store/store.js";
+import AdminLayout from "./AdminLayout.jsx";
+import { Login } from "./pages";
+import { AdminLogin , Analytics , ProductListing } from "./pages/admin";
+import NotFound from "./pages/admin/NotFound.jsx";
 
-const router=createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-    <Route path='/login' element={<Login/>}/>
-    <Route path="/"  element={<Layout />} >
-      <Route path='/' element={<>Logged in</>}/>
-    </Route>
-    <Route path='/admin/login' element={<AdminLogin/>}/>
-    <Route path='admin' element={<AdminLayout />}>
-      <Route path='' element={<>Admin</>}/>
-    </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<>Logged in</>} />
+      </Route>
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="admin/dashboard" element={<AdminLayout />} >
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="product-listings" element={<ProductListing/>} />
+          <Route path="users" element={<>Users</>} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </>
   )
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
