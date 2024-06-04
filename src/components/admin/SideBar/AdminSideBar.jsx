2b232d0d11c3607602360aae8cf3f693679db3b7
AliAais <aaisali228@gmail.com>
2024-06-04 12:13:23 -0700

    making admin dashboard responsive

--- src/components/admin/SideBar/AdminSideBar.jsx
+++ src/components/admin/SideBar/AdminSideBar.jsx
@@ -1,22 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { FaUsers } from "react-icons/fa6";
 import { AiFillProduct } from "react-icons/ai";
 import { IoAnalyticsOutline } from "react-icons/io5";
 import { MdOutlineStore } from "react-icons/md";
 
-  const AdminSideBar = () => {
-    return (
-      <aside className="bg-gray-800 sticky hidden md:block top-0 h-screen ">
-        <header className="px-4 py-3">
-          <div className="text-white text-2xl flex items-center justify-between  font-semibold">Double'A Ventures <MdOutlineStore style={{fontSize:'2.4rem'}} /></div>
-        </header>
-        <nav className="px-4 py-3 gap-y-4 font-semibold text-lg">
-          <Link to="./analytics" className=" w-[70%] flex justify-between items-center gap-x-4  mt-4 hover:text-white text-gray-300">Analytics <IoAnalyticsOutline style={{fontSize:'2rem'}} /> </Link>
-          <Link to="./product-listings" className=" w-[70%] justify-between flex items-center gap-x-4 mt-4 hover:text-white text-gray-300">Product Listing <AiFillProduct style={{fontSize:'2rem'}}/></Link>
-          <Link to="./users" className="w-[70%] justify-between mt-4 flex items-center gap-x-4 hover:text-white text-gray-300">Users <FaUsers style={{fontSize:'2rem'}}/></Link>
-        </nav>
-      </aside>
-    );
-  }
+const AdminSideBar = () => {
+  return (
+    <aside className="bg-gray-800 sticky hidden md:block top-0 h-screen ">
+      <header className="px-4 py-3">
+        <div className="text-white text-2xl flex items-center justify-between  font-semibold">
+          Double'A Ventures <MdOutlineStore style={{ fontSize: "2.4rem" }} />
+        </div>
+      </header>
+      <nav className="px-4 py-3 gap-y-4 font-semibold text-lg">
+        <Link
+          to="./analytics"
+          className=" w-[90%] flex justify-between items-center gap-x-4  mt-4 hover:text-white text-gray-300"
+        >
+          Analytics <IoAnalyticsOutline style={{ fontSize: "2rem" }} />{" "}
+        </Link>
+        <Link
+          to="./product-listings"
+          className=" w-[90%] justify-between flex items-center gap-x-4 mt-4 hover:text-white text-gray-300"
+        >
+          Products <AiFillProduct style={{ fontSize: "2rem" }} />
+        </Link>
+        <Link
+          to="./users"
+          className="w-[90%] justify-between mt-4 flex items-center gap-x-4 hover:text-white text-gray-300"
+        >
+          Users <FaUsers style={{ fontSize: "2rem" }} />
+        </Link>
+      </nav>
+    </aside>
+  );
+};
 
-  export default AdminSideBar;
+export default AdminSideBar;
--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { AdminSideBar } from '../components/admin';
-
+import { AdminSideBar , ResponsiveMenu } from '../components/admin';
+import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from 'react'
+import { RxCross2 } from "react-icons/rx";
 
 const AdminLayout = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="flex">
       <div className="md:w-[20%]">
@@ -10,7 +13,27 @@ const AdminLayout = () => {
       </div>
       <div className="w-[100%] px-6 py-3 md:[80%] min-h-screen bg-slate-200">
         <Outlet />
+        <div className={`${showMenu ? 'hidden':''} md:hidden cursor-pointer text-4xl text-gray-800 font-bold absolute top-4 right-4`}
+        onClick={()=>setShowMenu(!showMenu)}>
+          <AiOutlineMenu /> 
+        </div>
       </div>
+
+      {/* Responsive */}
+      
+      {
+        showMenu && (
+          <div className="md:hidden absolute h-screen bg-[rgba(19,19,19,0.71)] backdrop-blur-sm w-screen">
+            <div className="md:hidden cursor-pointer pointer text-4xl text-white font-bold absolute top-4 right-4"
+            onClick={()=>setShowMenu(!showMenu)}>
+              <RxCross2 /> 
+            </div>
+
+            <ResponsiveMenu setShowMenu={setShowMenu} showMenu={showMenu} />
+            
+          </div>
+        )
+      }
     </div>
   );
 }
