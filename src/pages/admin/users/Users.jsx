20ab9c38efb87134e55979d5079ecb68a64987a1
AliAais <aaisali228@gmail.com>
2024-06-19 14:06:42 -0700

    fetching data from server

--- src/pages/admin/users/Users.jsx
+++ src/pages/admin/users/Users.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import axios from 'axios'
 const Users = () => {
-    const users = [
-        { id: 1, name: 'John Doe', email: 'john@example.com' , phone:"0312345678"},
-        { id: 2, name: 'Jane Smith', email: 'jane@example.com' , phone:"0312345678" },
-        { id: 3, name: 'John Doe', email: 'john@example.com' , phone:"0312345678"},
-        { id: 4, name: 'Jane Smith', email: 'jane@example.com' , phone:"0312345678" },
-        { id: 5, name: 'John Doe', email: 'john@example.com' , phone:"0312345678"},
-        { id: 6, name: 'Jane Smith', email: 'jane@example.com' , phone:"0312345678" },
-        { id: 7, name: 'John Doe', email: 'john@example.com' , phone:"0312345678"},
-        { id: 8, name: 'Jane Smith', email: 'jane@example.com' , phone:"0312345678" },
-        { id: 9, name: 'John Doe', email: 'john@example.com' , phone:"0312345678"},
-        { id: 10, name: 'Jane Smith', email: 'jane@example.com' , phone:"0312345678" },
-        { id: 11, name: 'John Doe', email: 'john@example.com' , phone:"0312345678"},
-        { id: 12, name: 'Jane Smith', email: 'jane@example.com' , phone:"0312345678" },
-    ];
+    const [users,setUsers]=React.useState([]);
 
+     const handleDelete=async(id)=>{
+        try {
+            console.log('try')
+            const {data}=await axios.delete('http://localhost:3000/user/'+id);
+            setUsers(users.filter((user)=>user.id!==id))
+        } catch (error) {
+            console.log(error)
+        }
+     }
+     
+
+     useEffect(()=>{
+        
+    const fetchData=async()=>{
+        try {
+            const {data}=await axios.get('http://localhost:3000/user/getusers');
+            setUsers(data)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+     fetchData();
+     },[])
     return (
         <div className="container mx-auto">
             <h1 className="text-2xl font-bold mb-4">Users</h1>
@@ -29,14 +40,18 @@ const Users = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {users.map((user) => (
-                        <tr key={user.id} className='text-left'>
+                    { users.length>0 ?
+                        users?.map((user) => (
+                        <tr key={user.id} className="text-left space-y-2">
                             <td className="py-2 px-4 border-b">{user.id}</td>
-                            <td className="py-2 px-4 border-b">{user.name}</td>
+                            <td className="py-2 px-4 border-b">{user.name || 'Ali' }</td>
                             <td className="py-2 px-4 border-b">{user.email}</td>
-                            <td className="py-2 px-4 border-b">{user.phone}</td>
+                            <td className="py-2 px-4 border-b">{user.phone || '03123456789'}</td>
+                            <button className='border hover:bg-red-500 hover:text-white active:scale-[0.90] duration-75 border-red-500 font-semibold rounded-md text-red-500 p-2' onClick={()=>handleDelete(user.id)}>Delete Users</button>
                         </tr>
-                    ))}
+                    ))
+                    :<tr><td className='p-3 font-bold text-2xl text-red-500' >No Users</td></tr>
+                    }
                 </tbody>
             </table>
         </div>
