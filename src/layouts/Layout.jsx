9dfaab971d09dbafc102b530b417eb24e104da04
AliAais <aaisali228@gmail.com>
2024-07-04 08:35:48 -0700

    adding Toaster to top of app

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,15 +1,17 @@
 import { Header,Footer } from "../components"
 import { Outlet } from "react-router-dom"
 import Cart from "../modals/Cart"
+import { Toaster } from "react-hot-toast"
 
 const Layout = () => {
   return (
-    <>
+    <div className="overflow-hidden">
+      <Toaster/>
       <Header />
       <Cart/>
       <Outlet />
       <Footer />
-    </>
+    </div>
   )
 }
 
